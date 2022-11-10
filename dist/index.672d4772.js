0f259f41360a1c703a9fd5f18fff2b08992d755a
chijason99 <chijason99@gmail.com>
2022-11-10 21:34:18 +0000

    add gitignore

---
+++ dist/index.672d4772.js
@@ -0,0 +1,7 @@
+// if there is items in the shopping cart, show the shopping cart button
+// click on the shopping cart button will show the list of items in the cart
+// click on the cross would remove the item
+// the cart will show on different pages if there is any thing inside 
+// clicking add to cart in store would add item to the cart
+
+//# sourceMappingURL=index.672d4772.js.map
