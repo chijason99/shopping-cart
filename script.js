d6e229e67f14aa19a69e9705b543f697ffb787fa
chijason99 <chijason99@gmail.com>
2022-11-10 21:40:05 +0000

    initial commit

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // if there is items in the shopping cart, show the shopping cart button
-// click on the shopping cart button will show the list of items in the cart
+// click on the shopping cart button will toggle the list of items in the cart
 // click on the cross would remove the item
 // the cart will show on different pages if there is any thing inside 
 // clicking add to cart in store would add item to the cart
