faae184302ed4af2544c5f62ba2c7a12f7ed1100
chijason99 <chijason99@gmail.com>
2022-11-13 20:05:36 +0000

    using template tags instead of creating the elements in the DOM one by one

--- script.js
+++ script.js
@@ -28,6 +28,10 @@ const cartListItem = document.querySelector("[data-cart-list-item");
 const cartTotal = document.querySelector("[data-cart-list-total");
 const storeList = document.querySelector("[data-store-list]");
 
+const templateStoreItem = document.querySelector('[data-template-store-item]')
+const templateCartItem = document.querySelector('[data-template-cart-item]')
+console.log(templateCartItem)
+
 loadCart();
 displayCartBtn();
 if (shoppingCart.length !== 0) {
@@ -66,94 +70,38 @@ cartBtn.addEventListener("click", () => {
 //render the store list items with items.json
 
 function renderStoreItems() {
-  storeList.replaceChildren("");
   storeItems.forEach((item) => {
-    const itemContainer = document.createElement("div");
-    itemContainer.classList.add("lg:w-1/4", "md:w-1/2", "p-4", "w-full");
-    itemContainer.dataset.itemContainer = "";
-    const imageContainer = document.createElement("div");
-    imageContainer.classList.add(
-      "block",
-      "relative",
-      "h-48",
-      "rounded",
-      "overflow-hidden"
-    );
-    const image = document.createElement("img");
-    image.classList.add(
-      "object-cover",
-      "object-center",
-      "w-full",
-      "h-full",
-      "block"
-    );
-    image.setAttribute("alt", "ecommerce");
-    image.setAttribute(
+    const template = templateStoreItem.content.cloneNode(true)
+    const imageElement = template.querySelector('[data-image]')
+    const categoryElement = template.querySelector('[data-category]')
+    const nameElement = template.querySelector('[data-name]')
+    const priceElement = template.querySelector('[data-price]')
+    const addBtn = template.querySelector('[data-add-btn]')
+    imageElement.setAttribute(
       "src",
       `https://dummyimage.com/420x260/${item.imageColor}/${item.imageColor}`
     );
-    imageContainer.appendChild(image);
-    itemContainer.appendChild(imageContainer);
-    const detailsContainer = document.createElement("div");
-    detailsContainer.classList.add(
-      "mt-4",
-      "flex",
-      "items-end",
-      "justify-between"
-    );
-    detailsContainer.dataset.detailsContainer = "";
-    detailsContainer.dataset.color = item.imageColor;
-    const details = document.createElement("div");
-    const category = document.createElement("h3");
-    category.classList.add(
-      "text-gray-500",
-      "text-xs",
-      "tracking-widest",
-      "title-font",
-      "uppercase",
-      "mb-1"
-    );
-    category.innerText = item.category;
-    category.dataset.category = "";
-    const name = document.createElement("h2");
-    name.classList.add("text-gray-900", "title-font", "text-lg", "font-medium");
-    name.innerText = item.name;
-    name.dataset.name = "";
-    const price = document.createElement("p");
-    price.classList.add("mt-1");
-    price.innerText = `$${parseFloat((item.priceCents / 100).toFixed(2))}`;
-    price.dataset.price = parseFloat((item.priceCents / 100).toFixed(2));
-    const addToCartBtn = document.createElement("button");
-    addToCartBtn.classList.add(
-      "text-white",
-      "py-2",
-      "px-4",
-      "text-xl",
-      "bg-blue-500",
-      "rounded",
-      "hover:bg-blue-700"
-    );
-    addToCartBtn.innerText = "Add to Cart";
-    addToCartBtn.dataset.add = "";
-    details.appendChild(category);
-    details.appendChild(name);
-    details.appendChild(price);
-    detailsContainer.appendChild(details);
-    detailsContainer.appendChild(addToCartBtn);
-    itemContainer.appendChild(detailsContainer);
-    storeList.appendChild(itemContainer);
+    imageElement.dataset.color = item.imageColor;
+    categoryElement.innerText = item.category;
+    categoryElement.dataset.category = item.category;
+    nameElement.innerText = item.name;
+    nameElement.dataset.name = item.name;
+    priceElement.innerText = `$${parseFloat((item.priceCents / 100).toFixed(2))}`;
+    priceElement.dataset.price = parseFloat((item.priceCents / 100).toFixed(2));
+    addBtn.innerText = "Add to Cart";
+    storeList.appendChild(template);
   });
 }
 
 // clicking add to cart in store would add item to the cart
 
 document.addEventListener("click", (e) => {
-  if (!e.target.matches("[data-add]")) return;
+  if (!e.target.matches("[data-add-btn]")) return;
   const detailsContainer = e.target.closest("[data-details-container]");
-  const itemName = detailsContainer.querySelector("[data-name]").innerText;
+  const itemName = detailsContainer.querySelector("[data-name]").dataset.name;
   const itemPrice =
     detailsContainer.querySelector("[data-price]").dataset.price;
-  const itemColor = detailsContainer.dataset.color;
+  const itemColor = detailsContainer.querySelector("[data-image]").dataset.color;
   const cartItem = {
     itemId: uuidv4(),
     itemName,
@@ -165,7 +113,6 @@ document.addEventListener("click", (e) => {
   showCartLength();
   saveCart();
   renderCart();
-  console.log(shoppingCart);
   // thinking about the structure of the shopping cart
   //array, use filter method?
 });
@@ -174,92 +121,33 @@ document.addEventListener("click", (e) => {
 function renderCart() {
   cartListItem.replaceChildren("");
   shoppingCart.forEach((item) => {
+    const template = templateCartItem.content.cloneNode(true)
+    const cartItemContainer = template.querySelector('[data-cart-item-container]')
+    const imageElement = template.querySelector('[data-cart-item-image]')
+    const priceElement = template.querySelector('[data-cart-item-price]')
+    const nameElement = template.querySelector('[data-cart-item-name]')
+    const numberElement = template.querySelector('[data-cart-item-number]')
+
     const num = (shoppingCart.filter((i) => i.itemColor === item.itemColor)).length;
     const firstId = shoppingCart.find(i => i.itemColor === item.itemColor).itemId
     if(num > 1 && item.itemId !== firstId) return
-    const newCartListItemContainer = document.createElement("div");
-    newCartListItemContainer.classList.add("mb-6");
-    newCartListItemContainer.dataset.id = item.itemId;
-    const newCartListItemImageContainer = document.createElement("div");
-    newCartListItemImageContainer.classList.add(
-      "block",
-      "relative",
-      "h-24",
-      "rounded",
-      "overflow-hidden"
-    );
-    const newCartListImage = document.createElement("img");
-    newCartListImage.setAttribute("alt", "ecommerce");
-    newCartListImage.classList.add(
-      "object-cover",
-      "object-center",
-      "w-full",
-      "h-full",
-      "block",
-      "rounded"
-    );
-    newCartListImage.setAttribute(
+    cartItemContainer.dataset.id = item.itemId;
+    imageElement.setAttribute(
       "src",
       `https://dummyimage.com/210x130/${item.itemColor}/${item.itemColor}`
     );
-    const newRemoveBtn = document.createElement("button");
-    newRemoveBtn.dataset.removeFromCartButton = "";
-    newRemoveBtn.classList.add(
-      "absolute",
-      "top-0",
-      "right-0",
-      "bg-black",
-      "rounded-tr",
-      "text-white",
-      "w-6",
-      "h-6",
-      "text-lg",
-      "flex",
-      "justify-center",
-      "items-center"
-    );
-    newRemoveBtn.innerText = " \xD7 ";
-    newCartListItemImageContainer.appendChild(newCartListImage);
-    newCartListItemImageContainer.appendChild(newRemoveBtn);
-    const newCartListItemDetailsContainer = document.createElement("div");
-    newCartListItemDetailsContainer.classList.add(
-      "mt-2",
-      "flex",
-      "justify-between"
-    );
-    const newCartListItemTextContainer = document.createElement("div");
-    newCartListItemTextContainer.classList.add(
-      "flex",
-      "items-center",
-      "title-font"
-    );
-    const newItemName = document.createElement("h2");
-    newItemName.classList.add("text-gray-900", "text-lg", "font-medium");
-    newItemName.innerText = item.itemName;
-    const newItemPrice = document.createElement("div");
-    newItemPrice.innerText = `$${parseInt(item.itemPrice).toFixed(2)}`;
-    newCartListItemTextContainer.appendChild(newItemName);
+
+    nameElement.innerText = item.itemName;
+    priceElement.innerText = `$${parseInt(item.itemPrice).toFixed(2)}`;
     if (num > 1) {
-      const newItemNumber = document.createElement("span");
-      newItemNumber.classList.add(
-        "text-gray-600",
-        "text-sm",
-        "font-bold",
-        "ml-1"
-      );
-      newItemNumber.innerText = `x${num}`;
-      newCartListItemTextContainer.appendChild(newItemNumber)
+      numberElement.innerText = `x${num}`;
     }
-    newCartListItemDetailsContainer.appendChild(newCartListItemTextContainer);
-    newCartListItemDetailsContainer.appendChild(newItemPrice);
-    newCartListItemContainer.appendChild(newCartListItemImageContainer);
-    newCartListItemContainer.appendChild(newCartListItemDetailsContainer);
-    cartListItem.append(newCartListItemContainer);
     const sum = shoppingCart.reduce(
       (accumulator, i) => accumulator + parseInt(i.itemPrice),
       0
     );
     cartTotal.innerText = `$${sum.toFixed(2)}`;
+    cartListItem.appendChild(template)
   });
 }
 //removing the item from cart
@@ -278,3 +166,4 @@ document.addEventListener("click", (e) => {
     showCartLength()
   }
 });
+
